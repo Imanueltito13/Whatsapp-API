70d29b46add9ab173e688d926ea15b2b2922916d
Imanueltito13 <imanueltito21@gmail.com>
2024-12-12 15:22:49 +0700

    instalasi tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/**/*.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tambahkan semua file React Anda
+  ],
   theme: {
     extend: {},
   },
