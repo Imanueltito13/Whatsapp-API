299e3c20510b905e4bfd809b1237f553dd7a1644
Imanueltito13 <imanueltito21@gmail.com>
2024-12-28 08:50:31 +0700

    update UI

--- client/src/App.js
+++ client/src/App.js
@@ -111,7 +111,7 @@ const Template = ({ onSelectTemplate, selectedTemplate }) => {
       <h1 className="text-xl sm:text-3xl font-bold text-center mb-6">
         Template Pesan Facebook
       </h1>
-      <div className="grid grid-cols-1  md:grid-cols-3  gap-4">
+      <div className="grid grid-cols-1  md:grid-cols-4  gap-4">
         {templates.map((template, index) => (
           <div
             key={template.id}
